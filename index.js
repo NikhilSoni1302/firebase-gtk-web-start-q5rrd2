e48d42e67bea82d4830d2927944fd286f0b08508
Meinfinity <soninikhil098@gmail.com>
2021-08-27 16:02:31 +0530

    2 files modified

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import './style.css';
 import { initializeApp } from 'firebase/app';
 
 // Add the Firebase products and methods that you want to use
-import {} from 'firebase/auth';
+import { getAuth, EmailAuthProvider } from 'firebase/auth';
 import {} from 'firebase/firestore';
 
 import * as firebaseui from 'firebaseui';
@@ -27,10 +27,24 @@ let db, auth;
 
 async function main() {
   // Add Firebase project configuration object here
-  const firebaseConfig = {};
+
+const firebaseConfig = {
+  apiKey: "AIzaSyARfaKdFww8DjNJfgaLdg0WnL4m6HI5TYU",
+  authDomain: "fir-web-codelab-d6d01.firebaseapp.com",
+  projectId: "fir-web-codelab-d6d01",
+  storageBucket: "fir-web-codelab-d6d01.appspot.com",
+  messagingSenderId: "772675890534",
+  appId: "1:772675890534:web:6f40fd85c7cbc43e9a6ca7",
+  measurementId: "G-1RBX8JL1RF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
   // initializeApp(firebaseConfig);
-
+  initializeApp(firebaseConfig);
+  auth = getAuth();
   // FirebaseUI config
   const uiConfig = {
     credentialHelper: firebaseui.auth.CredentialHelper.NONE,
@@ -48,5 +62,10 @@ async function main() {
   };
 
   // const ui = new firebaseui.auth.AuthUI(auth);
+  const ui = new firebaseui.auth.AuthUI(auth);
+  startRsvpButton.addEventListener("click",
+   () => {
+        ui.start("#firebaseui-auth-container", uiConfig);
+  });
 }
 main();
